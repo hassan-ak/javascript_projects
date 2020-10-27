5422fdcb4616f525d592f8f7913fa207dffb72ad
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-27 13:11:04 +0500

    Update script.js
    Updated JS to track confirm password.

--- p01/script.js
+++ p01/script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const confirm_password = document.getElementById('confirm_password');
+const confirm_password = document.getElementById('confirmPassword');
 
 // Function to show error
 function showError(input,message) {
