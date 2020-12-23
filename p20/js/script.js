12b502c80340308d20a6904d10cdcf626bdc23b3
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-12-23 10:12:29 +0500

    Final Project (20) Create Header
    1:- Modified
    	a:- p20\index.html to create header component in the app
    	b:- p20\css\style.css for styling header component
    	c:- p20\js\script.js
    		i:- activate theme change button to toggle between dark and light mode
    		ii:- open and close menu bar on click
    		iii:- highlight header bar when scrolling down
    		iv:- Hide menu once option is clicked

---
+++ p20/js/script.js
@@ -0,0 +1,59 @@
+// ***---***---***//
+//  Dark Light Theme
+// get dom elements
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'darkTheme'
+const iconTheme = 'bx-sun'
+// Check for theme from the last visit
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+// Obtain current theme
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+// Check if page already visited and selected some theme
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+// ***---***---***//
+//  Show and hide menu on Click
+// function for showing menu
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('showMenu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+// Hide menu when option is clicked
+const navLink = document.querySelectorAll('.navLink')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each navLink, we remove the show-menu class
+    navMenu.classList.remove('showMenu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+window.addEventListener("click", linkAction);
+
+// ***---***---***//
+// Header border while scroling down
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 200) nav.classList.add('scrollHeader'); else nav.classList.remove('scrollHeader')
+}
+window.addEventListener('scroll', scrollHeader)
+
